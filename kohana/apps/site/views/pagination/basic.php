7dc7efff9cb49c41228bd65a49ed85e0c5145ed9
Dino Krtanjek <krtanjekdino@gmail.com>
2011-10-25 00:48:06 +0200

    Promjenjen način na koji paginacija prikazuje stranice

--- kohana/apps/site/views/pagination/basic.php
+++ kohana/apps/site/views/pagination/basic.php
@@ -1,37 +1,90 @@
-<p class="pagination">
+<?php
 
-	<?php if ($first_page !== FALSE): ?>
-		<a href="<?php echo HTML::chars($page->url($first_page)) ?>" rel="first">prva</a>
-	<?php else: ?>
-		<span>prva</span>
-	<?php endif ?>
+echo '
+<p class="pagination">';
 
-	<?php if ($previous_page !== FALSE): ?>
-		<a href="<?php echo HTML::chars($page->url($previous_page)) ?>" rel="prev">«</a>
-	<?php else: ?>
-		<span>«</span>
-	<?php endif ?>
+if ($first_page !== FALSE) {
+	echo '
+		<a href="'.HTML::chars($page->url($first_page)).'" rel="first">prva</a>';
+} else {
+	echo '
+		<span>prva</span>';
+}
 
-	<?php for ($i = 1; $i <= $total_pages; $i++): ?>
+if ($previous_page !== FALSE) {
+	echo '
+		<a href="'.HTML::chars($page->url($previous_page)).'" rel="prev">«</a>';
+} else {
+	echo '
+		<span>«</span>';
+}
 
-		<?php if ($i == $current_page): ?>
-			<span><?php echo $i ?></span>
-		<?php else: ?>
-			<a href="<?php echo HTML::chars($page->url($i)) ?>"><?php echo $i ?></a>
-		<?php endif ?>
+if ($total_pages > 9) {
+	$i = $current_page - 4;
+	$to = $current_page + 4;
+	$last_pages = 0;
 
-	<?php endfor ?>
+	if ($i < 1) {
+		$to += abs($i) + 1;
+		$i = 1;
+	} else {
+		if ($to > $total_pages) {
+			$i -= $to - $total_pages;
+			$to = $total_pages;
+		}
+	}
 
-	<?php if ($next_page !== FALSE): ?>
-		<a href="<?php echo HTML::chars($page->url($next_page)) ?>" rel="next">»</a>
-	<?php else: ?>
-		<span>»</span>
-	<?php endif ?>
+	if ($total_pages - $to > 2) {
+		$last_pages = 1;
+	}
 
-	<?php if ($last_page !== FALSE): ?>
-		<a href="<?php echo HTML::chars($page->url($last_page)) ?>" rel="last">zadnja</a>
-	<?php else: ?>
-		<span>zadnja</span>
-	<?php endif ?>
+	for (; $i <= $to; $i++) {
+		if ($i == $current_page) {
+			echo '
+		<span>'.$i.'</span>';
+		} else {
+			echo '
+		<a href="'.HTML::chars($page->url($i)).'">'.$i.'</a>';
+		}
+	}
 
-</p>
+	if ($last_pages) {
+		echo '
+		<span class="separator">…</span>';
+
+		for ($i = $total_pages - $last_pages; $i <= $total_pages; $i++) {
+			echo '
+		<a href="'.HTML::chars($page->url($i)).'">'.$i.'</a>';
+		}
+	}
+
+} else {
+	for ($i = 1; $i <= $total_pages; $i++) {
+		if ($i == $current_page) {
+			echo '
+		<span>'.$i.'</span>';
+		} else {
+			echo '
+		<a href="'.HTML::chars($page->url($i)).'">'.$i.'</a>';
+		}
+	}
+}
+
+if ($next_page !== FALSE) {
+	echo '
+		<a href="'.HTML::chars($page->url($next_page)).'" rel="next">»</a>';
+} else {
+	echo '
+		<span>»</span>';
+}
+
+if ($last_page !== FALSE) {
+	echo '
+		<a href="'.HTML::chars($page->url($last_page)).'" rel="last">zadnja</a>';
+} else {
+	echo '
+		<span>zadnja</span>';
+}
+
+echo '
+</p>';
